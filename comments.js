94d49a51d0ed365d9573fb80b09a388c3765dda6
Fanindra Saini <fanin.s.pbl@gmail.com>
2023-11-02 19:21:33 +0000

    Commit3

--- comments.js
+++ comments.js
@@ -1,15 +1,80 @@
 //Create Web server
-const express = require("express");
-const app = express();
-const port = 3000;
+const Comment = mongoose.model("Comment", commentSchema);
 
-//Create a connection to MongoDB
-const mongoose = require("mongoose");
-mongoose.connect("mongodb://localhost:27017/comments", { useNewUrlParser: true });
-const db = mongoose.connection;
+//Create a new comment
+const newComment = new Comment({
+    content: "I love this post!",
+    date: new Date()
+});
 
-//Create a schema
-const commentSchema = new mongoose.Schema({
-    content: String,
-    date: Date
+//Save the comment
+newComment.save(function (err, comment) {
+    if (err) return console.error(err);
+    console.log(comment);
+});
+
+//Query the comments
+Comment.find(function (err, comments) {
+    if (err) return console.error(err);
+    console.log(comments);
+});
+
+//Query one comment
+Comment.findOne({ content: "I love this post!" }, function (err, comment) {
+    if (err) return console.error(err);
+    console.log(comment);
+});
+
+//Update a comment
+Comment.findOneAndUpdate({ content: "I love this post!" }, { content: "I love this post so much!" }, function (err, comment) {
+    if (err) return console.error(err);
+    console.log(comment);
+});
+
+//Delete a comment
+Comment.findOneAndDelete({ content: "I love this post so much!" }, function (err, comment) {
+    if (err) return console.error(err);
+    console.log(comment);
+});
+
+//Create a new comment
+const newComment2 = new Comment({
+    content: "I love this post!",
+    date: new Date()
+});
+
+//Save the comment
+newComment2.save(function (err, comment) {
+    if (err) return console.error(err);
+    console.log(comment);
+});
+
+//Query the comments
+Comment.find(function (err, comments) {
+    if (err) return console.error(err);
+    console.log(comments);
+});
+
+//Query one comment
+Comment.findOne({ content: "I love this post!" }, function (err, comment) {
+    if (err) return console.error(err);
+    console.log(comment);
+});
+
+//Update a comment
+Comment.findOneAndUpdate({ content: "I love this post!" }, { content: "I love this post so much!" }, function (err, comment) {
+    if (err) return console.error(err);
+    console.log(comment);
+});
+
+//Delete a comment
+Comment.findOneAndDelete({ content: "I love this post so much!" }, function (err, comment) {
+    if (err) return console.error(err);
+    console.log(comment);
+});
+
+//Create a new comment
+const newComment3 = new Comment({
+    content: "I love this post!",
+    date: new Date()
 });
